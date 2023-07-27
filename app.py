716cb8c74f3ba28b58ffdd3c53dd8368deb93a79
260297 <robertomanica@yahoo.com.br>
2023-07-27 15:16:17 -0300

    otmização

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 class ContaBancaria:
+    LIMITE_SAQUES_DIARIOS = 3
+    LIMITE_SAQUE_DIARIO = 500
+
     def __init__(self):
-        self.saldo = 0
-        self.depositos = []
-        self.saques = []
+        self._saldo = 0
+        self._saques_realizados_hoje = 0
 
     def deposito(self, valor):
         if valor > 0:
-            self.saldo += valor
-            self.depositos.append(valor)
+            self._saldo += valor
             print(f"Depósito de R$ {valor:.2f} realizado com sucesso.")
         else:
             print("O valor do depósito deve ser positivo.")
 
     def saque(self, valor):
         if valor > 0:
-            if len(self.saques) < 3 and valor <= 500 and self.saldo >= valor:
-                self.saldo -= valor
-                self.saques.append(valor)
+            if self._saques_realizados_hoje < ContaBancaria.LIMITE_SAQUES_DIARIOS and valor <= ContaBancaria.LIMITE_SAQUE_DIARIO and self._saldo >= valor:
+                self._saldo -= valor
+                self._saques_realizados_hoje += 1
                 print(f"Saque de R$ {valor:.2f} realizado com sucesso.")
             else:
                 print("Não foi possível realizar o saque. Verifique o limite diário ou saldo disponível.")
@@ -24,22 +25,36 @@ class ContaBancaria:
             print("O valor do saque deve ser positivo.")
 
     def extrato(self):
-        if not self.depositos and not self.saques:
-            print("Não foram realizadas movimentações.")
+        print("Extrato:")
+        if self._saques_realizados_hoje == 0:
+            print("Não foram realizadas movimentações hoje.")
         else:
-            print("Extrato:")
-            for deposito in self.depositos:
-                print(f"Depósito: R$ {deposito:.2f}")
-            for saque in self.saques:
-                print(f"Saque: R$ {saque:.2f}")
-            print(f"Saldo atual: R$ {self.saldo:.2f}")
+            print("Saques realizados hoje:")
+            print(f"Número de saques: {self._saques_realizados_hoje}")
+        print(f"Saldo atual: R$ {self._saldo:.2f}")
 
 
-# Exemplo de uso:
 if __name__ == "__main__":
     conta = ContaBancaria()
-    conta.deposito(1000.50)
-    conta.saque(300.75)
-    conta.deposito(500.30)
-    conta.saque(200.25)
-    conta.extrato()
+
+    while True:
+        print("\n1 - Depósito")
+        print("2 - Saque")
+        print("3 - Extrato")
+        print("0 - Sair")
+
+        opcao = int(input("Escolha uma opção: "))
+
+        if opcao == 1:
+            valor_deposito = float(input("Digite o valor do depósito: "))
+            conta.deposito(valor_deposito)
+        elif opcao == 2:
+            valor_saque = float(input("Digite o valor do saque: "))
+            conta.saque(valor_saque)
+        elif opcao == 3:
+            conta.extrato()
+        elif opcao == 0:
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Digite novamente.")
